323954dab4b5d52f1aebdc72cf9980ff38031f10
Yosuke Ishikawa <y@ishkawa.org>
2015-02-18 23:15:30 +0900

    initial commit

---
+++ APIKit/APIKit.h
@@ -0,0 +1,4 @@
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double APIKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
---
+++ APIKit/APIKit.swift
@@ -0,0 +1,14 @@
+import Foundation
+import LlamaKit
+
+public protocol Request {
+    typealias Response: Any
+    
+    var URLRequest: NSURLRequest { get }
+    
+    func responseFromObject(object: AnyObject) -> Response?
+}
+
+public protocol API {
+    class func sendRequest<T: Request>(request: T, handler: (Result<T.Response, NSError>))
+}
