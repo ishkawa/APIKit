4cafc68be4a860b07551c5b4c2f1f9bfdb5766e3
Yosuke Ishikawa <y@ishkawa.org>
2015-02-20 22:41:26 +0900

    add URL to RequestBodyBuilder

--- APIKit/RequestBodyBuilder.swift
+++ APIKit/RequestBodyBuilder.swift
@@ -1,6 +1,8 @@
 import Foundation
 import LlamaKit
 
+public let APIKitRequestBodyBuidlerErrorDomain = "APIKitRequestBodyBuidlerErrorDomain"
+
 public enum RequestBodyBuilder {
     case JSON(NSJSONWritingOptions)
     case URL(NSStringEncoding)
@@ -11,6 +13,13 @@ public enum RequestBodyBuilder {
 
         switch self {
         case .JSON(let writingOptions):
+            if !NSJSONSerialization.isValidJSONObject(object) {
+                let userInfo = [NSLocalizedDescriptionKey: "invalidate object for JSON passed."]
+                let error = NSError(domain: APIKitRequestBodyBuidlerErrorDomain, code: 0, userInfo: userInfo)
+                result = Result.Failure(Box(error))
+                break
+            }
+            
             var error: NSError?
             if let data = NSJSONSerialization.dataWithJSONObject(object, options: writingOptions, error: &error) {
                 result = Result.Success(Box(data))
@@ -20,8 +29,26 @@ public enum RequestBodyBuilder {
             }
 
         case .URL(let encoding):
-            // FIXME:
-            result = Result.Success(Box(NSData()))
+            var queryItems = [NSURLQueryItem]()
+            
+            if let dictionary = object as? [String: AnyObject] {
+                for (key, value) in dictionary {
+                    let string = (value as? String) ?? "\(value)"
+                    let queryItem = NSURLQueryItem(name: key, value: string)
+                    queryItems.append(queryItem)
+                }
+            }
+            
+            let components = NSURLComponents()
+            components.queryItems = queryItems
+            
+            if let data = components.query?.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true) {
+                result = Result.Success(Box(data))
+            } else {
+                let userInfo = [NSLocalizedDescriptionKey: "failed to create url encoded dictionary."]
+                let error = NSError(domain: APIKitRequestBodyBuidlerErrorDomain, code: 0, userInfo: userInfo)
+                result = Result.Failure(Box(error))
+            }
 
         case .Custom(let buildBodyFromObject):
             result = buildBodyFromObject(object)
