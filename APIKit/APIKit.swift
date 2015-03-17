fb7bff12b913475e9628bd713b32bbfc78f88230
Yosuke Ishikawa <y@ishkawa.org>
2015-03-17 13:18:24 +0900

    Add responseErrorFromObject method to give a chance to create more detailed error using response

--- APIKit/APIKit.swift
+++ APIKit/APIKit.swift
@@ -166,9 +166,14 @@ public class API: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
                 
                 let statusCode = (URLResponse as? NSHTTPURLResponse)?.statusCode ?? 0
                 if !contains(self.acceptableStatusCodeRange, statusCode) {
-                    let userInfo = [NSLocalizedDescriptionKey: "received status code that represents error"]
-                    let error = NSError(domain: APIKitErrorDomain, code: statusCode, userInfo: userInfo)
-                    dispatch_async(mainQueue, { handler(.Failure(Box(error))) })
+                    var error: NSError = {
+                        switch self.responseBodyParser().parseData(data) {
+                        case .Success(let box): return self.responseErrorFromObject(box.unbox)
+                        case .Failure(let box): return box.unbox
+                        }
+                    }()
+
+                    dispatch_async(mainQueue) { handler(failure(error)) }
                     return
                 }
                 
@@ -180,8 +185,8 @@ public class API: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
                         let error = NSError(domain: APIKitErrorDomain, code: 0, userInfo: userInfo)
                         return failure(error)
                     }
-                    
                 }
+
                 dispatch_async(mainQueue, { handler(mappedResponse) })
             }
             
@@ -196,7 +201,13 @@ public class API: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
             return nil
         }
     }
-    
+
+    public class func responseErrorFromObject(object: AnyObject) -> NSError {
+        let userInfo = [NSLocalizedDescriptionKey: "received status code that represents error"]
+        let error = NSError(domain: APIKitErrorDomain, code: 0, userInfo: userInfo)
+        return error
+    }
+
     // MARK: - NSURLSessionTaskDelegate
     // TODO: add attributes like NS_REQUIRES_SUPER when it is available in future version of Swift.
     public func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError connectionError: NSError?) {
