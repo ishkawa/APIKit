57eb5288c54995b7ce513b6499527ef2dad62359
Yosuke Ishikawa <y@ishkawa.org>
2015-04-10 10:55:54 +0900

    use LlamaKit 0.6.0

--- APIKit/ResponseBodyParser.swift
+++ APIKit/ResponseBodyParser.swift
@@ -25,14 +25,14 @@ public enum ResponseBodyParser {
     public func parseData(data: NSData) -> Result<AnyObject, NSError> {
         switch self {
         case .JSON(let readingOptions):
-            return try { error in
+            return try ({ error in
                 return NSJSONSerialization.JSONObjectWithData(data, options: readingOptions, error: error)
-            }
+            })
 
         case .URL(let encoding):
-            return try { error in
+            return try ({ error in
                 return URLEncodedSerialization.objectFromData(data, encoding: encoding, error: error)
-            }
+            })
 
         case .Custom(let (accept, parseData)):
             return parseData(data)
