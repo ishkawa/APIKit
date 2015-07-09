367d6a5e0a24e3fef2738b3984cae2a438ac00df
Syo Ikeda <suicaicoca@gmail.com>
2015-07-09 11:59:36 +0900

    Updates for Xcode 7 beta 3
    > If an element of an enum with string raw type does not have an explicit raw value, it will default to the text of the enum’s name.

--- APIKit/HTTPMethod.swift
+++ APIKit/HTTPMethod.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 public enum HTTPMethod: String {
-    case GET = "GET"
-    case POST = "POST"
-    case PUT = "PUT"
-    case HEAD = "HEAD"
-    case DELETE = "DELETE"
-    case PATCH = "PATCH"
-    case TRACE = "TRACE"
-    case OPTIONS = "OPTIONS"
-    case CONNECT = "CONNECT"
+    case GET
+    case POST
+    case PUT
+    case HEAD
+    case DELETE
+    case PATCH
+    case TRACE
+    case OPTIONS
+    case CONNECT
 }
