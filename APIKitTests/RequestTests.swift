59650cc48505835f9dfdb57596deadd26b126eae
Yosuke Ishikawa <y@ishkawa.org>
2015-08-21 00:31:27 +0900

    Move dummy type for testing

--- APIKitTests/RequestTests.swift
+++ APIKitTests/RequestTests.swift
@@ -3,37 +3,37 @@ import OHHTTPStubs
 import APIKit
 
 class RequestTests: XCTestCase {
+    struct SearchRequest: MockAPIRequest {
+        let query: String
+        
+        // MARK: Request
+        typealias Response = [String: AnyObject]
+        
+        var method: HTTPMethod {
+            return .GET
+        }
+        
+        var path: String {
+            return "/"
+        }
+        
+        var parameters: [String: AnyObject] {
+            return [
+                "q": query,
+            ]
+        }
+        
+        func responseFromObject(object: AnyObject, URLResponse: NSHTTPURLResponse) -> Response? {
+            return object as? [String: AnyObject]
+        }
+    }
+
     override func tearDown() {
         OHHTTPStubs.removeAllStubs()
         super.tearDown()
     }
 
     func testURLQueryParameterEncoding() {
-        struct SearchRequest: MockAPIRequest {
-            let query: String
-            
-            // MARK: Request
-            typealias Response = [String: AnyObject]
-            
-            var method: HTTPMethod {
-                return .GET
-            }
-            
-            var path: String {
-                return "/"
-            }
-            
-            var parameters: [String: AnyObject] {
-                return [
-                    "q": query,
-                ]
-            }
-            
-            func responseFromObject(object: AnyObject, URLResponse: NSHTTPURLResponse) -> Response? {
-                return object as? [String: AnyObject]
-            }
-        }
-
         OHHTTPStubs.stubRequestsPassingTest({ request in
             XCTAssert(request.URL?.query == "q=%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF")
             return true
