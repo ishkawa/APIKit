4c47529c6729f1e00fb8e179c5bfbdc15b8fa5e4
Yosuke Ishikawa <y@ishkawa.org>
2016-03-19 12:00:00 +0900

    Add URLFormEncodedDataParser

---
+++ Sources/DataParserType/URLFormEncodedDataParser.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+/// A type that parses URL encoded response data.
+public class URLFormEncodedDataParser: DataParserType {
+    public enum Error: ErrorType {
+        case CannotGetStringFromData(NSData)
+    }
+
+    public let encoding: NSStringEncoding
+
+    public init(encoding: NSStringEncoding) {
+        self.encoding = encoding
+    }
+
+    // MARK: DataParserType
+    public var contentType: String? {
+        return "application/x-www-form-urlencoded"
+    }
+
+    public func parseData(data: NSData) throws -> AnyObject {
+        guard let string = NSString(data: data, encoding: encoding) as? String else {
+            throw Error.CannotGetStringFromData(data)
+        }
+
+        let components = NSURLComponents()
+        components.percentEncodedQuery = string
+
+        let queryItems = components.queryItems ?? []
+        var dictionary = [String: AnyObject]()
+
+        for queryItem in queryItems {
+            dictionary[queryItem.name] = queryItem.value
+        }
+
+        return dictionary
+    }
+}
