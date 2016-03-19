fa8b1e1d5105fd42758f99c9e99dc15570c3de4d
Yosuke Ishikawa <y@ishkawa.org>
2016-03-19 23:32:48 +0900

    :pencil: Error

--- Sources/Error/RequestError.swift
+++ Sources/Error/RequestError.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// `RequestError` represents a common error that occurs while building `NSURLRequest` from `RequestType`.
 enum RequestError: ErrorType {
     case InvalidBaseURL(NSURL)
     case UnexpectedURLRequest(NSURLRequest)
--- Sources/Error/ResponseError.swift
+++ Sources/Error/ResponseError.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// `ResponseError` represents a common error that occurs while getting `RequestType.Response`
+/// from raw result tuple `(NSData?, NSURLResponse?, NSError?)`.
 public enum ResponseError: ErrorType {
     case NonHTTPURLResponse(NSURLResponse?)
     case UnacceptableStatusCode(Int)
