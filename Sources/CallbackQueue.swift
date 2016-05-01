3dccf9b0b7b49afca347df035a02a4086a8c18c6
Yosuke Ishikawa <y@ishkawa.org>
2016-05-01 15:22:35 +0900

    :pencil: CallbackQueue

--- Sources/CallbackQueue.swift
+++ Sources/CallbackQueue.swift
@@ -1,9 +1,17 @@
 import Foundation
 
+/// `CallbackQueue` represents queue where `handler` of `Session.sendRequest(_:handler:)` runs.
 public enum CallbackQueue {
+    /// Dispatches callback closure on main queue asynchronously.
     case Main
+
+    /// Dispatches callback closure on the queue where backend adapter callback runs.
     case SessionQueue
+
+    /// Dispatches callback closure on associated operation queue.
     case OperationQueue(NSOperationQueue)
+
+    /// Dispatches callback closure on associated dispatch queue.
     case DispatchQueue(dispatch_queue_t)
 
     internal func execute(closure: () -> Void) {
