f4ece4b26779ed369873c776e251ecbc934dcf2c
Yosuke Ishikawa <y@ishkawa.org>
2016-05-25 00:03:57 +0900

    Not use completionHandler parameter of NSURLSession.dataTaskWithRequest method

--- Sources/SessionAdapterType/NSURLSessionAdapter.swift
+++ Sources/SessionAdapterType/NSURLSessionAdapter.swift
@@ -26,7 +26,7 @@ public class NSURLSessionAdapter: NSObject, SessionAdapterType, NSURLSessionDele
 
     /// Creates `NSURLSessionDataTask` instance using `dataTaskWithRequest(_:completionHandler:)`.
     public func createTaskWithURLRequest(URLRequest: NSURLRequest, handler: (NSData?, NSURLResponse?, ErrorType?) -> Void) -> SessionTaskType {
-        let task = URLSession.dataTaskWithRequest(URLRequest, completionHandler: handler)
+        let task = URLSession.dataTaskWithRequest(URLRequest)
 
         setBuffer(NSMutableData(), forTask: task)
         setHandler(handler, forTask: task)
