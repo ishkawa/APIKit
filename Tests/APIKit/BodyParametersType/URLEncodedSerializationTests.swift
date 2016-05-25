5e111ec986ce93f39dd009113c8085fc07f2f7ef
Yosuke Ishikawa <y@ishkawa.org>
2016-05-25 21:56:32 +0900

    Add tests for URLEncodedSerialization

---
+++ Tests/APIKit/BodyParametersType/URLEncodedSerializationTests.swift
@@ -0,0 +1,56 @@
+import Foundation
+import XCTest
+import APIKit
+
+class URLEncodedSerializationTests: XCTestCase {
+    // MARK: NSData -> AnyObject
+    func testObjectFromData() {
+        let data = "key1=value1&key2=value2".dataUsingEncoding(NSUTF8StringEncoding)!
+        let object = try? URLEncodedSerialization.objectFromData(data, encoding: NSUTF8StringEncoding)
+        XCTAssertEqual(object?["key1"], "value1")
+        XCTAssertEqual(object?["key2"], "value2")
+    }
+
+    func testInvalidFormatString() {
+        let string = "key==value&"
+
+        do {
+            let data = string.dataUsingEncoding(NSUTF8StringEncoding)!
+            try URLEncodedSerialization.objectFromData(data, encoding: NSUTF8StringEncoding)
+            XCTFail()
+        } catch {
+            guard let error = error as? URLEncodedSerialization.Error,
+                  case .InvalidFormatString(let invalidString) = error else {
+                XCTFail()
+                return
+            }
+
+            XCTAssertEqual(string, invalidString)
+        }
+    }
+
+    // MARK: AnyObject -> NSData
+    func testDataFromObject() {
+        let object = ["hey": "yo"] as AnyObject
+        let data = try? URLEncodedSerialization.dataFromObject(object, encoding: NSUTF8StringEncoding)
+        let string = data.flatMap { NSString(data: $0, encoding: NSUTF8StringEncoding) }
+        XCTAssertEqual(string, "hey=yo")
+    }
+
+    func testNonDictionaryObject() {
+        let dictionaries = [["hey": "yo"]] as AnyObject
+
+        do {
+            try URLEncodedSerialization.dataFromObject(dictionaries, encoding: NSUTF8StringEncoding)
+            XCTFail()
+        } catch {
+            guard let error = error as? URLEncodedSerialization.Error,
+                  case .CannotCastObjectToDictionary(let object) = error else {
+                XCTFail()
+                return
+            }
+
+            XCTAssertEqual(object["hey"], dictionaries["hey"])
+        }
+    }
+}
