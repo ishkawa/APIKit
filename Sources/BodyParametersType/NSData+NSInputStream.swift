c59610be67293b6519f870eb758c8e866de85625
Syo Ikeda <suicaicoca@gmail.com>
2016-08-08 23:20:01 +0900

    Distinguish between `Swift.min()` and `Sequence.min()`

--- Sources/BodyParametersType/NSData+NSInputStream.swift
+++ Sources/BodyParametersType/NSData+NSInputStream.swift
@@ -9,7 +9,7 @@ extension Data {
     init(inputStream: InputStream, capacity: Int = Int(UInt16.max)) throws {
         var data = Data(capacity: capacity)
 
-        let bufferSize = _min(Int(UInt16.max), capacity)
+        let bufferSize = Swift.min(Int(UInt16.max), capacity)
         let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
 
         var readSize: Int
@@ -33,8 +33,4 @@ extension Data {
 
         self.init(data)
     }
-}
-
-private func _min(_ x: Int, _ y: Int) -> Int {
-    return y < x ? y : x
 }
