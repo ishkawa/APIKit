46e9e7b421ffe8cbaf2a3723a37d241d807dc901
Yosuke Ishikawa <y@ishkawa.org>
2016-09-19 01:59:32 +0900

    Add old protocols with availabity attributes

---
+++ Sources/Deprecated.swift
@@ -0,0 +1,25 @@
+//
+//  Deprecated.swift
+//  APIKit
+//
+//  Created by Yosuke Ishikawa on 9/19/16.
+//  Copyright © 2016 Yosuke Ishikawa. All rights reserved.
+//
+
+
+// MARK: - Protocols
+
+@available(*, deprecated, renamed: "Request")
+public typealias RequestType = Request
+
+@available(*, deprecated, renamed: "SessionAdapter")
+public typealias SessionAdapterType = SessionAdapter
+
+@available(*, deprecated, renamed: "SessionTask")
+public typealias SessionTaskType = SessionTask
+
+@available(*, deprecated, renamed: "BodyParameters")
+public typealias BodyParametersType = BodyParameters
+
+@available(*, deprecated, renamed: "DataParser")
+public typealias DataParserType = DataParser
