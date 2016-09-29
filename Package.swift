390b5962f321fe28347d1dc2dcda918b8f1e9702
Yosuke Ishikawa <y@ishkawa.org>
2016-09-30 00:22:50 +0900

    Fix project layout for swift build

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "APIKit",
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 1),
+        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3),
     ]
 )
