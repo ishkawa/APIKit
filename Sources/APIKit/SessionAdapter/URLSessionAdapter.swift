74bb262058f9f11b0298cfd085f10ad29841a959
Yosuke Ishikawa <y@ishkawa.org>
2016-10-01 19:13:54 +0900

    Replace objc dependent implementation from URLSessionAdapter

--- Sources/APIKit/SessionAdapter/URLSessionAdapter.swift
+++ Sources/APIKit/SessionAdapter/URLSessionAdapter.swift
@@ -4,9 +4,6 @@ extension URLSessionTask: SessionTask {
 
 }
 
-private var dataTaskResponseBufferKey = 0
-private var taskAssociatedObjectCompletionHandlerKey = 0
-
 /// `URLSessionAdapter` connects `URLSession` with `Session`.
 ///
 /// If you want to add custom behavior of `URLSession` by implementing delegate methods defined in
@@ -15,6 +12,18 @@ private var taskAssociatedObjectCompletionHandlerKey = 0
 /// `URLSession(_:task: didCompleteWithError:)` and `URLSession(_:dataTask:didReceiveData:)`, you have to call
 /// `super` in these methods if you implement them.
 open class URLSessionAdapter: NSObject, SessionAdapter, URLSessionDelegate, URLSessionTaskDelegate, URLSessionDataDelegate {
+    internal final class TaskProperty {
+        var buffer: Data
+        let handler: (Data?, URLResponse?, Error?) -> Void
+        
+        init(buffer: Data = Data(), handler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+            self.buffer = buffer
+            self.handler = handler
+        }
+    }
+
+    internal var taskProperties = [Int: TaskProperty]()
+
     /// The undelying `URLSession` instance.
     open var urlSession: URLSession!
 
@@ -27,9 +36,7 @@ open class URLSessionAdapter: NSObject, SessionAdapter, URLSessionDelegate, URLS
     /// Creates `URLSessionDataTask` instance using `dataTaskWithRequest(_:completionHandler:)`.
     open func createTask(with URLRequest: URLRequest, handler: @escaping (Data?, URLResponse?, Error?) -> Void) -> SessionTask {
         let task = urlSession.dataTask(with: URLRequest)
-
-        setBuffer(NSMutableData(), forTask: task)
-        setHandler(handler, forTask: task)
+        taskProperties[task.taskIdentifier] = TaskProperty(handler: handler)
 
         return task
     }
@@ -45,29 +52,17 @@ open class URLSessionAdapter: NSObject, SessionAdapter, URLSessionDelegate, URLS
         }
     }
 
-    private func setBuffer(_ buffer: NSMutableData, forTask task: URLSessionTask) {
-        objc_setAssociatedObject(task, &dataTaskResponseBufferKey, buffer, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-
-    private func buffer(for task: URLSessionTask) -> NSMutableData? {
-        return objc_getAssociatedObject(task, &dataTaskResponseBufferKey) as? NSMutableData
-    }
-
-    private func setHandler(_ handler: @escaping (Data?, URLResponse?, Error?) -> Void, forTask task: URLSessionTask) {
-        objc_setAssociatedObject(task, &taskAssociatedObjectCompletionHandlerKey, handler as Any, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-
-    private func handler(for task: URLSessionTask) -> ((Data?, URLResponse?, Error?) -> Void)? {
-        return objc_getAssociatedObject(task, &taskAssociatedObjectCompletionHandlerKey) as? (Data?, URLResponse?, Error?) -> Void
-    }
-
     // MARK: URLSessionTaskDelegate
     open func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        handler(for: task)?(buffer(for: task) as Data?, task.response, error)
+        let taskProperty = taskProperties[task.taskIdentifier]
+        taskProperty?.handler(taskProperty?.buffer, task.response, error)
+
+        taskProperties[task.taskIdentifier] = nil
     }
 
     // MARK: URLSessionDataDelegate
     open func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        buffer(for: dataTask)?.append(data)
+        let taskProperty = taskProperties[dataTask.taskIdentifier]
+        taskProperty?.buffer.append(data)
     }
 }
--- Tests/APIKitTests/SessionAdapterType/URLSessionAdapterTests.swift
+++ Tests/APIKitTests/SessionAdapterType/URLSessionAdapterTests.swift
@@ -1,8 +1,10 @@
 import Foundation
-import APIKit
 import XCTest
 
+@testable import APIKit
+
 class URLSessionAdapterTests: XCTestCase {
+    var adapter: URLSessionAdapter!
     var session: Session!
 
     override func setUp() {
@@ -11,7 +13,7 @@ class URLSessionAdapterTests: XCTestCase {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [HTTPStub.self]
         
-        let adapter = URLSessionAdapter(configuration: configuration)
+        adapter = URLSessionAdapter(configuration: configuration)
         session = Session(adapter: adapter)
     }
 
@@ -35,8 +37,10 @@ class URLSessionAdapterTests: XCTestCase {
             
             expectation.fulfill()
         }
-        
+
         waitForExpectations(timeout: 10.0, handler: nil)
+        
+        XCTAssert(adapter.taskProperties.isEmpty)
     }
     
     func testConnectionError() {
@@ -65,6 +69,8 @@ class URLSessionAdapterTests: XCTestCase {
         }
         
         waitForExpectations(timeout: 10.0, handler: nil)
+        
+        XCTAssert(adapter.taskProperties.isEmpty)
     }
 
     func testCancel() {
@@ -91,5 +97,7 @@ class URLSessionAdapterTests: XCTestCase {
         }
 
         waitForExpectations(timeout: 10.0, handler: nil)
+        
+        XCTAssert(adapter.taskProperties.isEmpty)
     }
 }
