5cb7d5d0f1590c2052f15d90b7ee66114db512a3
kitasuke <yusuke2759@gmail.com>
2016-11-22 13:17:26 -0800

    Fix test case for protobuf data parser

--- Tests/APIKitTests/DataParserType/ProtobufDataParserTests.swift
+++ Tests/APIKitTests/DataParserType/ProtobufDataParserTests.swift
@@ -8,21 +8,16 @@ class ProtobufDataParserTests: XCTestCase {
         XCTAssertEqual(parser.contentType, "application/protobuf")
     }
     
-    func testJSONSuccess() {
-        let dictionary = [
-            "foo": 1,
-            "bar": 2,
-            "baz": 3
-        ]
-        let data = NSKeyedArchiver.archivedData(withRootObject: dictionary)
+    func testProtobufSuccess() {
+        let data = NSKeyedArchiver.archivedData(withRootObject: ["foo": 1, "bar": 2, "baz": 3])
         let parser = ProtobufDataParser()
         
         do {
             let object = try parser.parse(data: data) as! Data
-            let parsedDictionary = NSKeyedUnarchiver.unarchiveObject(with: object) as! [String: Int]
-            XCTAssertEqual(parsedDictionary["foo"], dictionary["foo"])
-            XCTAssertEqual(parsedDictionary["bar"], dictionary["bar"])
-            XCTAssertEqual(parsedDictionary["baz"], dictionary["baz"])
+            let dictionary = NSKeyedUnarchiver.unarchiveObject(with: object) as? [String: Int]
+            XCTAssertEqual(dictionary?["foo"], 1)
+            XCTAssertEqual(dictionary?["bar"], 2)
+            XCTAssertEqual(dictionary?["baz"], 3)
         } catch {
             XCTFail()
         }
