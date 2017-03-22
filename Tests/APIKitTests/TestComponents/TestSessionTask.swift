10eff1594d661919457eea822f508093da4443c0
taish <kbystis@gmail.com>
2017-03-22 22:18:34 +0900

    Add a way to get progress of uploading

--- Tests/APIKitTests/TestComponents/TestSessionTask.swift
+++ Tests/APIKitTests/TestComponents/TestSessionTask.swift
@@ -2,12 +2,14 @@ import Foundation
 import APIKit
 
 class TestSessionTask: SessionTask {
-    
+
     var handler: (Data?, URLResponse?, Error?) -> Void
+    var progressHandler: (Int64, Int64, Int64) -> Void
     var cancelled = false
 
-    init(handler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    init(progressHandler: @escaping  (Int64, Int64, Int64) -> Void, handler: @escaping (Data?, URLResponse?, Error?) -> Void) {
         self.handler = handler
+        self.progressHandler = progressHandler
     }
 
     func resume() {
