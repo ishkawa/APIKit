87488209692fb1e90b074b83f50943733ccf8254
taish <kbystis@gmail.com>
2017-03-25 17:24:34 +0900

    rename argment `progressHandler` to `completionHandler` in `TestSessionTask`

--- Tests/APIKitTests/TestComponents/TestSessionTask.swift
+++ Tests/APIKitTests/TestComponents/TestSessionTask.swift
@@ -3,12 +3,12 @@ import APIKit
 
 class TestSessionTask: SessionTask {
 
-    var handler: (Data?, URLResponse?, Error?) -> Void
+    var completionHandler: (Data?, URLResponse?, Error?) -> Void
     var progressHandler: (Int64, Int64, Int64) -> Void
     var cancelled = false
 
-    init(progressHandler: @escaping  (Int64, Int64, Int64) -> Void, handler: @escaping (Data?, URLResponse?, Error?) -> Void) {
-        self.handler = handler
+    init(progressHandler: @escaping  (Int64, Int64, Int64) -> Void, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        self.completionHandler = completionHandler
         self.progressHandler = progressHandler
     }
 
