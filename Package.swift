c3562f37db2bc7cd3c4e6d12b354cb1d7e6ce9a2
Econa77 <s.f.1992.ip@gmail.com>
2022-01-01 11:48:36 +0900

    Change iOS deployment target version

--- Package.swift
+++ Package.swift
@@ -1,22 +1,11 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-var platforms: [SupportedPlatform] {
-    #if compiler(<5.3)
-        return [
-            .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
-        ]
-    #else
-        // Xcode 12 (which ships with Swift 5.3) drops support for iOS 8
-        return [
-            .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
-        ]
-    #endif
-}
-
 let package = Package(
     name: "APIKit",
-    platforms: platforms,
+    platforms: [
+        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         .library(name: "APIKit", targets: ["APIKit"]),
     ],
