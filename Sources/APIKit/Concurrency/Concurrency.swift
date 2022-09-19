eebbc027901b450cb4522fd403f669a0b02f78d8
Econa77 <s.f.1992.ip@gmail.com>
2022-09-20 02:00:10 +0900

    Rename SessionTask Actor to SessionTaskCancellationHandler

--- Sources/APIKit/Concurrency/Concurrency.swift
+++ Sources/APIKit/Concurrency/Concurrency.swift
@@ -3,42 +3,45 @@
 import Foundation
 
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-private actor SessionTaskActor {
-    private weak var sessionTask: SessionTask?
-
-    func send(_ sessionTask: SessionTask?) {
-        self.sessionTask = sessionTask
-    }
-
-    func cancel() {
-        sessionTask?.cancel()
-    }
-}
-
-@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 public extension Session {
     static func response<Request: APIKit.Request>(for request: Request, callbackQueue: CallbackQueue? = nil) async throws -> Request.Response {
         return try await shared.response(for: request, callbackQueue: callbackQueue)
     }
 
     func response<Request: APIKit.Request>(for request: Request, callbackQueue: CallbackQueue? = nil) async throws -> Request.Response {
-        let sessionTaskActor = SessionTaskActor()
+        let cancellationHandler = SessionTaskCancellationHandler()
         return try await withTaskCancellationHandler(operation: {
             return try await withUnsafeThrowingContinuation { continuation in
                 Task {
-                    await sessionTaskActor.send(send(request, callbackQueue: callbackQueue) { result in
+                    let sessionTask = send(request, callbackQueue: callbackQueue) { result in
                         switch result {
                         case .success(let response):
                             continuation.resume(returning: response)
                         case .failure(let error):
                             continuation.resume(throwing: error)
                         }
-                    })
+                    }
+                    await cancellationHandler.register(with: sessionTask)
                 }
             }
         }, onCancel: {
-            Task { await sessionTaskActor.cancel() }
+            Task { await cancellationHandler.cancel() }
         })
+    }
+}
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+private actor SessionTaskCancellationHandler {
+    private var sessionTask: SessionTask?
+    
+    func register(with task: SessionTask?) {
+        guard sessionTask == nil else { return }
+        sessionTask = task
+    }
+
+    func cancel() {
+        sessionTask?.cancel()
+        sessionTask = nil
     }
 }
 
