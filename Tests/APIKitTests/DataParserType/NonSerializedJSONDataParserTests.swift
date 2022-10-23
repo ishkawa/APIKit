58fc67ca86a4c2fa2afa4d35e1aaebeba97818f7
Econa77 <s.f.1992.ip@gmail.com>
2022-10-23 15:23:35 +0900

    Rename DecodableJSONDataParser to NonSerializedJSONDataParser

--- Tests/APIKitTests/DataParserType/NonSerializedJSONDataParserTests.swift
+++ Tests/APIKitTests/DataParserType/NonSerializedJSONDataParserTests.swift
@@ -2,15 +2,15 @@ import XCTest
 import APIKit
 import XCTest
 
-class DecodableJSONDataParserTests: XCTestCase {
+class NonSerializedJSONDataParserTests: XCTestCase {
     func testContentType() {
-        let parser = DecodableJSONDataParser()
+        let parser = NonSerializedJSONDataParser()
         XCTAssertEqual(parser.contentType, "application/json")
     }
 
     func testJSONSuccess() throws {
         let data = try XCTUnwrap("data".data(using: .utf8))
-        let parser = DecodableJSONDataParser()
+        let parser = NonSerializedJSONDataParser()
 
         let object = try parser.parse(data: data)
         XCTAssertEqual(object, data)
